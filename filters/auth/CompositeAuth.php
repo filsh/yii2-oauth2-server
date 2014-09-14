5fa45811dcd60ffbddd96ff92e97029a7d8bf79d
Igor Maliy <imaliy.filsh@gmail.com>
2014-09-15 01:20:58 +0300

    Fix after update

--- filters/auth/CompositeAuth.php
+++ filters/auth/CompositeAuth.php
@@ -6,14 +6,15 @@ use \Yii;
 
 class CompositeAuth extends \yii\filters\auth\CompositeAuth
 {
-    public function authenticate($user, $request, $response)
+    /**
+     * @inheritdoc
+     */
+    public function beforeAction($action)
     {
-        $oauthsServer = Yii::$app->getModule('oauth2')->getServer();
+        $oauthServer = Yii::$app->getModule('oauth2')->getServer();
         $oauthRequest = Yii::$app->getModule('oauth2')->getRequest();
-        if ($oauthsServer->verifyResourceRequest($oauthRequest)) {
-            return parent::authenticate($user, $request, $response);
-        }
+        $oauthServer->verifyResourceRequest($oauthRequest);
         
-        return null;
+        return parent::beforeAction($action);
     }
 }
